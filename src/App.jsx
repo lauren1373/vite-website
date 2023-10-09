62d0d96eae8bbd2a1a698e57659f907a0f2cc4f9
Lauren <lfleming1373@gmail.com>
2023-10-08 22:04:57 -0400

    added stuff

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Navbar } from './components'
+ 
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
